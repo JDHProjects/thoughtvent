e3d1b3cc71430f220c695c0bb6c920bd28cbae0b
Joshua Purdy <josh@codesigned.co.uk>
2019-07-31 23:26:58 +0100

    test

--- src/App.js
+++ src/App.js
@@ -60,7 +60,7 @@ const styles = {
   input: {
     textAlign: 'center',
     backgroundColor: '#282F30',
-    fontSize: 20,
+    fontSize: 40,
     color: '#FFFFFF',
     borderWidth: 0,
     width: '30%',
